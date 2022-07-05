9113f628aec7d4eb6bcec1fe2af2689a6c1dfe5b
Shan Konduru <ShanKonduru@users.noreply.github.com>
2022-07-05 22:15:53 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const webdriver = require("selenium-webdriver");
+const chrome = require("selenium-webdriver/chrome");
+const { By, Key } = require("selenium-webdriver");
+
+const chromeOptions = new chrome.Options();
+chromeOptions.addArguments("start-maximized");
+
+async function sessionFour() {
+  let driver = new webdriver.Builder()
+    .forBrowser("chrome")
+    .setChromeOptions(chromeOptions)
+    .build();
+
+  driver.get("https://www.facebook.com/");
+
+  userName = await driver.findElement(By.name("email"));
+  userName.sendKeys("shankonduru@gmail.com");
+
+  password = driver.findElement(By.name("pass"));
+  password.sendKeys("shankonduru@");
+
+  loginButton = driver.findElement(By.name("login"));
+  loginButton.click();
+}
+
+sessionFour();
